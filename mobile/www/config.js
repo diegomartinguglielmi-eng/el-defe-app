d76c7045f0750ca7da927d213e9b551ecf61d31e
diegomartinguglielmi-eng <diegomartinguglielmi@gmail.com>
2026-09-05 04:07:37 +0000

    Connect Android app to production API

--- mobile/www/config.js
+++ mobile/www/config.js
@@ -1,2 +1,2 @@
 // Pegar aquí la URL HTTPS de producción luego del deploy:
-window.EL_DEFE_API_URL = "https://REEMPLAZAR-CON-URL-REAL";
+window.EL_DEFE_API_URL = "https://el-defe-api-deploy-production.up.railway.app";
