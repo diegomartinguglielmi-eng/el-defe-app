2e7f6de1cb5e78c4ed4da63dbb2fe138c7aa7b9c
diegomartinguglielmi-eng <diegomartinguglielmi@gmail.com>
2026-09-06 15:01:59 -0300

    Expose verified FEFI category results API

--- app/main_v5.py
+++ app/main_v5.py
@@ -1,11 +1,13 @@
 from .main import app, scheduler
 from .pending import router
+from .fefi_results import router as fefi_results_router
 from .db import SessionLocal
 from .models import User
 from .auth import hash_password
 from .config import settings
 
 app.include_router(router)
+app.include_router(fefi_results_router)
 
 @app.on_event("startup")
 def v5_startup_hardening():
