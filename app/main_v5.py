3f0f6fd2647d896c63516fbeb1135a85f97fa7ef
diegomartinguglielmi-eng <diegomartinguglielmi@gmail.com>
2026-09-08 20:50:05 -0300

    Activar registro público y administración de usuarios

--- app/main_v5.py
+++ app/main_v5.py
@@ -14,6 +14,7 @@ from .notifications_v5 import router as notifications_router
 from .data_quality import router as data_quality_router
 from .home_v5 import router as home_router
 from .store_v5 import router as store_router, bootstrap_store
+from .user_admin import router as user_admin_router
 from .db import SessionLocal
 from .models import User, Match, SyncRun
 from .auth import hash_password
@@ -23,7 +24,7 @@ from .argenliga_baseline import bootstrap_argenliga_2026
 from .fefi_mayores import sync_fefi_mayores_b
 from .superliga_sync import sync_superliga
 
-app.include_router(router);app.include_router(fefi_results_router);app.include_router(fefi_schedules_router);app.include_router(fefi_freshness_router);app.include_router(notifications_router);app.include_router(data_quality_router);app.include_router(home_router);app.include_router(store_router)
+app.include_router(router);app.include_router(fefi_results_router);app.include_router(fefi_schedules_router);app.include_router(fefi_freshness_router);app.include_router(notifications_router);app.include_router(data_quality_router);app.include_router(home_router);app.include_router(store_router);app.include_router(user_admin_router)
 BASE=Path(__file__).resolve().parent
 LAAMBA_BOOTSTRAP_LOCK=2026090701;ARGENLIGA_BOOTSTRAP_LOCK=2026090702;FEFI_BOOTSTRAP_LOCK=2026090703;FEFI_MAYORES_BOOTSTRAP_LOCK=2026090704;SUPERLIGA_BOOTSTRAP_LOCK=2026090705
 
