f4be911efa8d61022c813c2f2658fdd348f4d103
diegomartinguglielmi-eng <diegomartinguglielmi@gmail.com>
2026-09-11 10:21:50 -0300

    Registrar estadísticas públicas de ligas

--- app/main_v5.py
+++ app/main_v5.py
@@ -18,6 +18,7 @@ from .store_v5 import router as store_router, bootstrap_store
 from .user_admin import router as user_admin_router
 from .sponsors_v5 import router as sponsors_router, bootstrap_sponsors
 from .league_tournaments import router as league_tournaments_router
+from .league_stats import router as league_stats_router
 from .db import SessionLocal
 from .models import User, Match, SyncRun
 from .auth import hash_password
@@ -28,7 +29,7 @@ from .argenliga_sync import sync_argenliga
 from .fefi_mayores import sync_fefi_mayores_b
 from .superliga_sync import sync_superliga
 
-app.include_router(router);app.include_router(fefi_results_router);app.include_router(fefi_schedules_router);app.include_router(fefi_freshness_router);app.include_router(fefi_standings_router);app.include_router(notifications_router);app.include_router(data_quality_router);app.include_router(home_router);app.include_router(store_router);app.include_router(user_admin_router);app.include_router(sponsors_router);app.include_router(league_tournaments_router)
+app.include_router(router);app.include_router(fefi_results_router);app.include_router(fefi_schedules_router);app.include_router(fefi_freshness_router);app.include_router(fefi_standings_router);app.include_router(notifications_router);app.include_router(data_quality_router);app.include_router(home_router);app.include_router(store_router);app.include_router(user_admin_router);app.include_router(sponsors_router);app.include_router(league_tournaments_router);app.include_router(league_stats_router)
 BASE=Path(__file__).resolve().parent
 LAAMBA_BOOTSTRAP_LOCK=2026090701;ARGENLIGA_BOOTSTRAP_LOCK=2026090702;FEFI_BOOTSTRAP_LOCK=2026090703;FEFI_MAYORES_BOOTSTRAP_LOCK=2026090704;SUPERLIGA_BOOTSTRAP_LOCK=2026090705
 
