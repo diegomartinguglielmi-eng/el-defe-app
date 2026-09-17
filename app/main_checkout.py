2f96b16bff0fd18ce4bd885146c4e94b620b03d8
diegomartinguglielmi-eng <diegomartinguglielmi@gmail.com>
2026-09-17 17:53:23 -0300

    Registra contexto familiar compartido

--- app/main_checkout.py
+++ app/main_checkout.py
@@ -5,6 +5,7 @@ from .availability_v1 import router as availability_router
 from .availability_player_v2 import router as availability_player_v2_router
 from .family_manage_v1 import router as family_manage_router
 from .family_catalog_v1 import router as family_catalog_router
+from .family_context_v1 import router as family_context_router
 
 app.include_router(store_submit_router)
 # Debe registrarse antes del router histórico: FastAPI resuelve la primera ruta coincidente.
@@ -14,6 +15,7 @@ app.include_router(availability_router)
 app.include_router(availability_player_v2_router)
 app.include_router(family_manage_router)
 app.include_router(family_catalog_router)
+app.include_router(family_context_router)
 
 @app.middleware('http')
 async def log_frontend_origin(request, call_next):
